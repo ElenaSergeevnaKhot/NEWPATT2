67b306c0b7669c9cc00a1de999899b6775aec78a
Elena <Elenakhot@mail.ru>
2024-01-22 15:36:37 +0300

    Fourth commit

--- src/test/java/ru/netology/delivery/data/CardDeliveryTest.java
+++ src/test/java/ru/netology/delivery/data/CardDeliveryTest.java
@@ -36,8 +36,7 @@ class CardDeliveryTest {
         $("[data-test-id=agreement]").click();
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate))
+        $("[data-test-id='success-notification'] .notification__content").shouldHave(Condition.exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(Condition.visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
@@ -46,7 +45,7 @@ class CardDeliveryTest {
                 .shouldHave(Condition.text("У Вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(Condition.visible);
         $("[data-test-id='replan-notification'] button").click();
-        $("data-test-id='success-notification'] .notification__content")
+        $("[data-test-id='success-notification'] .notification__content")
                 .shouldHave(Condition.exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(Condition.visible);
 
